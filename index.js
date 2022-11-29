4dcd3fdf489afb681d0cecdebaf2a10f62a7b174
Argen96 <106479453+Argen96@users.noreply.github.com>
2022-11-29 23:12:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import typeOfTheContentFile from './typeOfElementsInFile.js'
+import readContentOfTheFile from './readFile.js';
+
+const fileContent = await readContentOfTheFile('file.txt');
+
+console.log(typeOfTheContentFile(fileContent))
