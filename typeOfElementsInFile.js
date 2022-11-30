16f84b4bee2b5ed6200cbba80c7e2b1daa924a38
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-11-30 13:52:37 +0100

    Update typeOfElementsInFile.js

--- typeOfElementsInFile.js
+++ typeOfElementsInFile.js
@@ -5,14 +5,16 @@ function typeOfTheContentFile(array){
   const numbersType = array.every((elements)=> typeof Number(elements) ==='number' && !isNaN(Number(elements)));
   const stringsType = array.every((elements)=>typeof (elements) ==='string' && isNaN(Number(elements)));
 
-   if(numbersType){
-   const sumoftheElemnts = array.reduce((acc,values)=>acc+Number(values),0);
-   return sumoftheElemnts; 
- } else if( stringsType){
-   return new Set(array);
-  }else{
-  throw new Error('The elements in the file should contain only strings or numbers');
- }
+   if(numbersType) {
+    const sumoftheElemnts = array.reduce((acc,values)=>acc+Number(values),0);
+    return sumoftheElemnts; 
+   } 
+   else if(stringsType) {
+    return new Set(array);
+   }
+   else{
+    throw new Error('The elements in the file should contain only strings or numbers');
+   }
 }
 
 export default typeOfTheContentFile;
