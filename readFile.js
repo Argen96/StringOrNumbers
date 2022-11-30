78561d13bfd1d2c6f6d9edf3154551f5421e382b
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-11-30 13:56:01 +0100

    Update readFile.js

--- readFile.js
+++ readFile.js
@@ -1,9 +1,10 @@
 import fsp from 'fs/promises';
 // In this folder we turn the file into the array
 async function readContentOfTheFile(file){
-const fileImportedContent = await fsp.readFile(file,'utf-8');
-const ContentInArray = fileImportedContent.split('\r\n');
-return ContentInArray;
+ const fileImportedContent = await fsp.readFile(file,'utf-8');
+ const ContentInArray = fileImportedContent.split('\r\n');
+
+ return ContentInArray;
 }
 
 export default readContentOfTheFile;
